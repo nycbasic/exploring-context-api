3650676349ff9b1ca636b138980268af4ffadf38
nycbasic <samuel.poon@baruchmail.cuny.edu>
2020-12-24 14:00:35 -0500

    created form with material ui and styled using js styles

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,12 +1,13 @@
 import React from 'react';
-
 import '../App.css';
 import NavBar from "./NavBar";
+import Form from "./Form"
 
 function App() {
 	return (
 	<React.Fragment>
     <NavBar />
+    <Form />
   </React.Fragment>
 	);
 }
